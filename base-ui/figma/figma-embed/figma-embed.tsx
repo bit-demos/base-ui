49e62d3982f1a5a7d7227bafdcb3edb50103fd9b
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-10-07 19:48:12 +0200

    improve figma component

--- base-ui/figma/figma-embed/figma-embed.tsx
+++ base-ui/figma/figma-embed/figma-embed.tsx
@@ -2,31 +2,13 @@ import React from 'react';
 import classNames from 'classnames';
 import styles from './figma-embed.module.scss';
 
-export type FigmaEmbedProps = {
-  /**
-   * src for figma embed. Click on Share button in Figma and click on 'Get embed code' and then copy the value of the src attribute from the iframe.
-   */
-  src: string;
-  /**
-   * add lazy loading
-   */
-  loading?: 'lazy' | 'eager';
-  /**
-   * width of iframe
-   */
-  width?: string;
-  /**
-   * height of iframe
-   */
-  height?: string;
-  /**
-   * allow figma embed to open in full screen
-   */
-  allowFullScreen?: boolean;
-} & React.IframeHTMLAttributes<HTMLIFrameElement>;
+export type FigmaEmbedProps =
+  {} & React.IframeHTMLAttributes<HTMLIFrameElement>;
 
 export function FigmaEmbed({ className, ...rest }: FigmaEmbedProps) {
-  return <iframe className={classNames(styles.iframe, className)} {...rest} />;
+  return (
+    <iframe {...rest} className={classNames(styles.iframe, className)}></iframe>
+  );
 }
 
 FigmaEmbed.defaultProps = {
