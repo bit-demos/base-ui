3f91fdb1258a35b7b92138d84549dcafce430365
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-12-09 13:16:33 +0100

    update link

--- base-ui/ui/link/link.composition.tsx
+++ base-ui/ui/link/link.composition.tsx
@@ -1,25 +1,4 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router';
 import { Link } from './link';
 
-export const BasicLink = () => (
-  <MemoryRouter>
-    <Link href="/about">About</Link>
-  </MemoryRouter>
-);
-
-export const PrimaryLink = () => (
-  <MemoryRouter>
-    <Link href="/details" buttonClass variation="primary">
-      Buy Now
-    </Link>
-  </MemoryRouter>
-);
-
-export const SecondaryLink = () => (
-  <MemoryRouter>
-    <Link href="/details" buttonClass variation="secondary">
-      Buy Now
-    </Link>
-  </MemoryRouter>
-);
+export const BasicLink = () => <Link href="/about">About</Link>;
